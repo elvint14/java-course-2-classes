02813e851dffb9b430fba0703553efe1c275b61a
elvintaghizade14 <elvintaghiyev184@gmail.com>
2020-12-12 17:14:59 +0400

    lesson 09 video record uploaded into youtube

---
+++ src/main/java/org/course/lesson09/hw/DivideTheApples1.java
@@ -0,0 +1,14 @@
+package org.course.lesson09.hw;
+
+import java.util.Scanner;
+
+public class DivideTheApples1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+
+    System.out.println(k / n);
+  }
+}
