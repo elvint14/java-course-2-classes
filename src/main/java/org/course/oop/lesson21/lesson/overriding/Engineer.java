a40d506d33d57c75072df2737d32d37535403822
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-01-30 11:22:54 +0400

    lesson #21 video record uploaded into youtube

---
+++ src/main/java/org/course/oop/lesson21/lesson/overriding/Engineer.java
@@ -0,0 +1,22 @@
+package org.course.oop.lesson21.lesson.overriding;
+
+public class Engineer extends Person {
+
+  private String spec;
+
+  public Engineer(String name, String surname, int age, String spec) {
+    super(name, surname, age);
+    this.spec = spec;
+  }
+
+  @Override
+  public void goHome() {
+    System.out.println("Engineer is going home...");
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Engineer[name='%s', surname='%s', age=%d, spec='%s'}",
+            super.getName(), super.getSurname(), super.getAge(), this.spec);
+  }
+}
---
+++ src/main/java/org/course/oop/lesson21/lesson/wrapper/WrapperTypesApp.java
@@ -0,0 +1,21 @@
+package org.course.oop.lesson21.lesson.wrapper;
+
+public class WrapperTypesApp {
+  // global - static
+  private static String global_counter;
+
+  // instance
+  private int age;
+
+  // local
+  void doOp(){
+    int a = 0;
+    System.out.println(a);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(WrapperTypesApp.global_counter);
+
+    System.out.println(new WrapperTypesApp().age);
+  }
+}
