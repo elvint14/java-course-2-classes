0e27f3b8539af30e6584cfce6d935b455b6ccb67
elvintaghizade <elvintaghiyev184@gmail.com>
2021-05-14 15:53:14 +0400

    lesson 44 topic added

---
+++ src/main/java/org/course/part05/lesson45/code/LibraryApp.java
@@ -0,0 +1,8 @@
+package org.course.part05.lesson45.code;
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        double pow = Math.pow(2, 3);
+        System.out.println(pow);
+    }
+}
